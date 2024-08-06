e1b11446f42928b21859e8a497e4b74f23a1ada1
Lucas Sales <sales_l@hotmail.com>
2024-08-06 17:46:03 -0300

    testing master protection

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import classes from "../../Projects/index";
 
 const Home = (): JSX.Element => {
-  return <div>Happy hacking!</div>;
+  return <div>Happy hacking</div>;
 };
 
 export default Home;
