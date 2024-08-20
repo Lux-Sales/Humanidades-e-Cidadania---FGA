b881350428550f1aabe97d6036aee8c2f217a039
Pedro-Henrique3 <pedrohenriquefreirerodrigues@gmail.com>
2024-08-20 00:23:28 -0300

    Componente card de trabalho

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,8 +1,14 @@
 import React from "react";
 import classes from "../../Projects/index";
+import ProjectCard from "../../components/ProjectCard";
 
 const Home = (): JSX.Element => {
-  return <div>Happy hacking!</div>;
+  return (
+    <div>
+      <ProjectCard></ProjectCard>
+      Happy hacking!
+    </div>
+  );
 };
 
 export default Home;
