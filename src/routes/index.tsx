dc776aa3aa610ffd687680965fc22657a64ee89f
Caio Duarte <caioduarte330@gmail.com>
2024-08-20 22:35:30 -0300

    implemented header component

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import Home from "../pages/Home";
+import Home from "../pages/Home/index";
+import About from "../pages/Home/"; //adicionar pagina sobre
+import Classes from "../pages/Home/"; //adicionar pagina turmas
+import Contact from "../pages/Home/"; //adicionar pagina contatos
 
 function RoutesComponent(): JSX.Element {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/classes" element={<Classes />} />
+      <Route path="/contact" element={<Contact />} />
     </Routes>
   );
 }
