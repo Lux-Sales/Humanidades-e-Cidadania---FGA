edb438fe8b7a7434daea0ef3ad1694ed79d1504e
Caio Duarte <caioduarte330@gmail.com>
2024-09-19 23:57:09 -0300

    conflicts resolved

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,14 +1,13 @@
 import React from "react";
 import classes from "../../Projects/index";
 import Header from "../../components/Header/Header";
-import ProjectCard from "../../components/ProjectCard"
-const Home = (): JSX.Element => {
 
+const Home = (): JSX.Element => {
   return (
-  <div>
+    <div>
       <Header></Header>
-      <ProjectCard></ProjectCard>
-  </div>);
+    </div>
+  );
 };
 
 export default Home;
