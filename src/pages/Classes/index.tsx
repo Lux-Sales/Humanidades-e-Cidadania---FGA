56a2799e5b41083344a03c4ee88ccde225a7abd2
Pedro-Henrique3 <pedrohenriquefreirerodrigues@gmail.com>
2024-09-20 00:19:11 -0300

    Classes page's started

--- src/pages/Classes/index.tsx
+++ src/pages/Classes/index.tsx
@@ -1,28 +1,30 @@
-import React from 'react';
-import { useLocation } from 'react-router-dom';
-import Header from '../../components/Header/Header';
+import React from "react";
+import { useLocation } from "react-router-dom";
+import Header from "../../components/Header/Header";
+import ProjectCards from "../../components/ProjectCards/ProjectCards";
+import ProjectsPresentation from "../../components/ProjectsPresentation/ProjectsPresentation";
 
 export default function Contact() {
-    const location = useLocation();
-    const pathname = location.pathname;
+  const location = useLocation();
+  const pathname = location.pathname;
 
-    if (pathname === '/classes') {
-        return (
-            <div>
-                <Header></Header>
-                <main className='container'>
-                olá, essa é a página das turmas
-                </main>
-            </div>
-        );
-    } else { 
-//pra ajudar na depuração
-        return (
-            <div>
-                <Header></Header>
-                <p>route actual: {pathname}</p>
-                <div>your code is wrong</div>
-            </div>
-        );
-    }
+  if (pathname === "/classes") {
+    return (
+      <div>
+        <Header></Header>
+        <ProjectsPresentation>
+          <ProjectCards />
+        </ProjectsPresentation>
+      </div>
+    );
+  } else {
+    //pra ajudar na depuração
+    return (
+      <div>
+        <Header></Header>
+        <p>route actual: {pathname}</p>
+        <div>your code is wrong</div>
+      </div>
+    );
+  }
 }
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,16 +1,13 @@
 import React from "react";
 import classes from "../../Projects/index";
 import Header from "../../components/Header/Header";
-import ProjectCards from "../../components/ProjectCards/ProjectCards";
-import ProjectsPresentation from "../../components/ProjectsPresentation/ProjectsPresentation";
+
 
 const Home = (): JSX.Element => {
   return (
     <div>
       <Header></Header>
-      <ProjectsPresentation>
-        <ProjectCards />
-      </ProjectsPresentation>
+      
     </div>
   );
 };
