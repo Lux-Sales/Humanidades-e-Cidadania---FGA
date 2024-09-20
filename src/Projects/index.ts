2ca8f71d831645b224dc9d6a008cf628d01e49ae
Pedro-Henrique3 <pedrohenriquefreirerodrigues@gmail.com>
2024-09-19 23:32:50 -0300

    Adicionar mais projetos na base de dados

--- src/Projects/index.ts
+++ src/Projects/index.ts
@@ -1,11 +1,12 @@
 interface Work {
+  id: number;
   name: string;
   students: string[];
   summary: string;
   link: string;
 }
 
-interface Year {
+export interface Year {
   firstSemester: Work[];
   secondSemester: Work[];
 }
@@ -18,6 +19,39 @@ const classes: Classes = {
   "2024": {
     firstSemester: [
       {
+        id: 1,
+        name: "example 1 semester 1",
+        students: ["maria", "roberta", "joao"],
+        summary:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque debitis magnam harum in voluptatibus voluptate optio dolor pariatur, porro fugit libero hic consectetur amet enim voluptates ea non reiciendis magni.",
+        link: "https://google.com.br",
+      },
+      {
+        id: 2,
+        name: "example 1 semester 1",
+        students: ["maria", "roberta", "joao"],
+        summary:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque debitis magnam harum in voluptatibus voluptate optio dolor pariatur, porro fugit libero hic consectetur amet enim voluptates ea non reiciendis magni.",
+        link: "https://google.com.br",
+      },
+      {
+        id: 3,
+        name: "example 1 semester 1",
+        students: ["maria", "roberta", "joao"],
+        summary:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque debitis magnam harum in voluptatibus voluptate optio dolor pariatur, porro fugit libero hic consectetur amet enim voluptates ea non reiciendis magni.",
+        link: "https://google.com.br",
+      },
+      {
+        id: 4,
+        name: "example 1 semester 1",
+        students: ["maria", "roberta", "joao"],
+        summary:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque debitis magnam harum in voluptatibus voluptate optio dolor pariatur, porro fugit libero hic consectetur amet enim voluptates ea non reiciendis magni.",
+        link: "https://google.com.br",
+      },
+      {
+        id: 5,
         name: "example 1 semester 1",
         students: ["maria", "roberta", "joao"],
         summary:
@@ -27,6 +61,7 @@ const classes: Classes = {
     ],
     secondSemester: [
       {
+        id: 1,
         name: "example 1 semester 2",
         students: ["maria", "roberta", "joao"],
         summary:
